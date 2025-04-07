651cd1e48d722c1208bd692fd98ec50a9ac2d42b
Jude mpoyo <mpoyojude0@gmail.com>
2025-04-07 20:04:18 +0200

    adding adding a database schemal and models : customer,invoice,invoiceLine,product and supplier

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -1,11 +1,17 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-  protected $table = 'customers'; 
-  protected $fillable = ['name', 'phone'];
+    protected $table = 'customers';
 
+    protected $fillable = ['name', 'phone'];
+
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class);
+    }
 }
