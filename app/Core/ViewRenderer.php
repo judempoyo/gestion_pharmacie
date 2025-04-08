c1b5afa8443ff417d78128744f7f2abaebcedad9
Jude mpoyo <mpoyojude0@gmail.com>
2025-04-08 11:43:09 +0200

    feat: enhance ViewRenderer to support optional layout usage in rendering views

--- app/Core/ViewRenderer.php
+++ app/Core/ViewRenderer.php
@@ -2,11 +2,16 @@
 namespace App\Core;
 
 trait ViewRenderer {
-    protected function render(string $layoutpath, string $viewPath, array $data = []) {
+    protected function render(string $layoutpath, string $viewPath, array $data = [], bool $useLayout = true) {
         extract($data);
         ob_start();
         require __DIR__ . "/../../app/Views/$viewPath.php";
         $content = ob_get_clean();
-        include __DIR__ . "/../../app/Views/layouts/$layoutpath.php";
+        
+        if ($useLayout) {
+            include __DIR__ . "/../../app/Views/layouts/$layoutpath.php";
+        } else {
+            echo $content;
+        }
     }
 }
